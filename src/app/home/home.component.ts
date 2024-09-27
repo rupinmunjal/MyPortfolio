744aa30367841a69890eeeb872f81032d35c77a1
Rupin Munjal <munjalru@sheridancollege.ca>
2024-09-27 12:06:40 -0400

    Added comments in home component and impoved the code

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,21 +7,23 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
+
 export class HomeComponent {
   constructor(private titleService: Title, private http: HttpClient) {
-    this.titleService.setTitle('Rupin Munjal | Home');
+    this.titleService.setTitle('Rupin Munjal | Home'); // Setting the page title in the constructor
   }
 
-  personal!: any;
+  personal!: any; // Variable to hold personal data
 
+  // Method to fetch content from a JSON file
   getContent() {
-    let url = 'assets/data/content.json';
+    let url = 'assets/data/content.json'; // URL to the JSON file
     this.http.get(url).subscribe((res) => {
-      this.personal = res;
+      this.personal = res; // Assigning the response to the personal variable
     });
   }
 
-  ngOnInit(){
-    this.getContent();
+  ngOnInit() {
+    this.getContent(); // Fetch content when the component initializes
   }
 }
